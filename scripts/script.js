f1ff87d8330bc1669808062c451eaa8529bc2d17
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-02-15 15:23:27 -0800

    added a function

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,33 @@ function handleKeyboardButtonPress(event){
 
     if(playerPadded === expectedAlphabet){
         console.log('you are a point');
+        console.log('you have pressed correctly', expectedAlphabet)
+
+        const currentScore = getTextElementValueById ('current-score');
+        // console.log(currentScore);
+        const updateScore = currentScore + 1;
+        setTextElementValueById('current-score', updateScore)
+
+
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseFloat (currentScoreText);
+        // console.log(currentScore);
+
+        // const newScore = currentScore + 1;
+
+        // currentScoreElement.innerText = newScore;
+
+
         continueGame();
         removeBackgroundColorById(expectedAlphabet); 
     }else{
-        console.log('you missed, you lost a life.')
+        console.log('you missed lost a life.');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife);
+
     }
 
 }
@@ -29,9 +52,9 @@ document.addEventListener('keyup', handleKeyboardButtonPress);
 
 function continueGame(){
     const alphabet = getARandomAlphabet ();
-    console.log ('Your random alphabet', alphabet);
+    // console.log ('Your random alphabet', alphabet);
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
-    setBgColorById(alphabet)
+    setBackgroundColorById(alphabet)
 }
